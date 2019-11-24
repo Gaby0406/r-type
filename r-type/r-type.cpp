3abc43d8603742ce89677a700e0f6d424a7342e4
Gaby0406 <gabriel.girard@epitech.eu>
2019-11-24 18:35:58 +0100

    second push

---
+++ r-type/r-type.cpp
@@ -0,0 +1,17 @@
+﻿// r-type.cpp : définit le point d'entrée de l'application.
+//
+
+#include <SFML/System.hpp>
+#include <iostream>
+
+int main()
+{
+	sf::Clock Clock;
+	while (Clock.GetElapsedTime() < 5.f)
+	{
+		std::cout << Clock.GetElapsedTime() << std::endl;
+		sf::Sleep(0.5f);
+	}
+
+	return 0;
+}
